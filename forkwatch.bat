db5d66c2d5b91e31fcd506ed15b0ebdefb6f70bd
Sebastian <qSebastiaNp@users.noreply.github.com>
2021-03-18 16:24:23 +0100

    No external binary anymore + fixed pause error

--- forkwatch.bat
+++ forkwatch.bat
@@ -1,13 +1,11 @@
 @ECHO OFF
-REM forkwatch.bat v1.0.3 - qSebastiaNp
+REM forkwatch.bat v1.0.4 - qSebastiaNp
 SETLOCAL EnableExtensions EnableDelayedExpansion
 
 REM config
 REM use hash of (blockheight - $SAFEMARGIN) to rule out orphan hashes
 SET SAFEMARGIN=50
 SET TRANSCENDENCE=transcendence-cli.exe
-REM download newest jq-win32.exe from here (https://stedolan.github.io/jq/download/) and put it in this same directory or enter the path
-SET JQ=jq-win32.exe
 SET IFTTTKEY=none
 
 REM no need to change anything from here --->
@@ -15,13 +13,6 @@ REM no need to change anything from here --->
 REM check if transcendence-cli file exists
 IF NOT EXIST %TRANSCENDENCE% (
 	ECHO * Could not find %TRANSCENDENCE%. Please check the configuration at the top of this script.
-	PAUSE
-	EXIT /b 1
-)
-
-REM check if jq file exists
-IF NOT EXIST %JQ% (
-	ECHO * Could not find %JQ%. Please check the configuration at the top of this script.
 	PAUSE
 	EXIT /b 1
 )
@@ -37,8 +28,13 @@ IF NOT "%ERRORLEVEL%" == "0" (
 	)
 )
 
-FOR /F "Delims=" %%A IN ('"curl -s https://explorer.teloscoin.org/ext/getmasternodecount"') DO SET "MNCOUNT=%%~A"
-FOR /F "Delims=" %%B IN ('"%TRANSCENDENCE% masternode count | %JQ% ."stable""') DO SET "LOCALMNCOUNT=%%~B"
+FOR /F "Delims=" %%A IN ('"CALL curl -s https://explorer.teloscoin.org/ext/getmasternodecount"') DO SET "MNCOUNT=%%~A"
+FOR /F "Delims=" %%B IN ('"CALL %TRANSCENDENCE% masternode count"') DO SET "LOCALMNCOUNT=!LOCALMNCOUNT!%%B"
+SET LOCALMNCOUNT=%LOCALMNCOUNT:"=%
+SET "LOCALMNCOUNT=%LOCALMNCOUNT:~2,-2%"
+SET "LOCALMNCOUNT=%LOCALMNCOUNT:: ==%"
+SET "%LOCALMNCOUNT:, =" & SET "%"
+SET LOCALMNCOUNT=%stable%
 
 REM check if explorer knows enough masternodes, ELSE it may be forked
 SET /A "x=%LOCALMNCOUNT% / 2"
@@ -49,17 +45,19 @@ IF %MNCOUNT% LSS %x% (
 )
 
 REM output blockhash overview
-FOR /F "Delims=" %%C IN ('"curl -s https://explorer.teloscoin.org/api/getblockcount"') DO SET "BLOCKHEIGHT=%%~C"
+FOR /F "Delims=" %%C IN ('"CALL curl -s https://explorer.teloscoin.org/api/getblockcount"') DO SET "BLOCKHEIGHT=%%~C"
 SET /A "SAFEBLOCKHEIGHT=%BLOCKHEIGHT% - %SAFEMARGIN%"
 
-FOR /F "Delims=" %%D IN ('"%TRANSCENDENCE% getblockhash %SAFEBLOCKHEIGHT%"') DO SET "LOCALHASH=%%~D"
+FOR /F "Delims=" %%D IN ('"CALL %TRANSCENDENCE% getblockhash %SAFEBLOCKHEIGHT%"') DO SET "LOCALHASH=%%~D"
 ECHO Local   : %LOCALHASH%
 
 @ping -n 2 localhost> nul
-FOR /F "Delims=" %%E IN ('"curl -s https://explorer.teloscoin.org/api/getblockhash?index=%SAFEBLOCKHEIGHT%"') DO SET "EXPLORERHASH=%%~E"
+FOR /F "Delims=" %%E IN ('"CALL curl -s https://explorer.teloscoin.org/api/getblockhash?index=%SAFEBLOCKHEIGHT%"') DO SET "EXPLORERHASH=%%~E"
 ECHO Explorer: %EXPLORERHASH%
 
-FOR /F "Delims=" %%F IN ('"curl -s https://telos.polispay.com/api/block-index/%SAFEBLOCKHEIGHT% | %JQ% ."blockHash""') DO SET "POLISHASH=%%~F"
+FOR /F "Delims=" %%F IN ('"CALL curl -s https://telos.polispay.com/api/block-index/%SAFEBLOCKHEIGHT%"') DO SET "POLISHASH=%%~F"
+SET "POLISHASH=%POLISHASH:~14,64%"
+
 ECHO PolisPay: %POLISHASH%
 ECHO.
 
@@ -98,7 +96,7 @@ IF "%EXPLORERHASH%" == "%LOCALHASH%" (
 	REM send push notification - read README.MD
 	IF NOT "%IFTTTKEY%" == "none" (
 		FOR /F "Delims=" %%G IN ('"hostname"') DO SET "HOSTNAME=%%~G"
-		curl -X POST -H "Content-Type: application/json" -d "{\"value1\":\"It seems %HOSTNAME% is FORKED.\"}" https://maker.ifttt.com/trigger/notify/with/key/%IFTTTKEY% >NUL
+		CALL curl -X POST -H "Content-Type: application/json" -d "{\"value1\":\"It seems %HOSTNAME% is FORKED.\"}" https://maker.ifttt.com/trigger/notify/with/key/%IFTTTKEY% >NUL
 	)
 	PAUSE
 	EXIT /b 1
